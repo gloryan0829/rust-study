f2f281155108a654234bd0f69de9136da666b744
gloryan0829 <candoublej@gmail.com>
2023-12-15 09:55:37 +0900

    hello, there

---
+++ hello/target/debug/deps/hello-26061bcc95c8f304.d
@@ -0,0 +1,5 @@
+/Users/motivator/Workspace/projects/rust-study/hello/target/debug/deps/hello-26061bcc95c8f304: src/main.rs
+
+/Users/motivator/Workspace/projects/rust-study/hello/target/debug/deps/hello-26061bcc95c8f304.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/motivator/Workspace/projects/rust-study/hello/target/debug/hello: /Users/motivator/Workspace/projects/rust-study/hello/src/main.rs
